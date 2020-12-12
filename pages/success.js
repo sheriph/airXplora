a51a57616a681fdd8d3068704fa4aff357da6164
sheriph <sheriph4real@gmail.com>
2020-12-12 18:45:18 +0100

    fixing errors

--- pages/success.js
+++ pages/success.js
@@ -11,6 +11,8 @@ import { Container, Grid } from "@material-ui/core";
 import FlightSumarry from "../components/flightresult/flightsummary";
 import BaseHeader from "../components/headers/baseheader";
 import ClassicFooter from "../components/footers/classicfooter";
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
 
 const styles = makeStyles((theme) => ({
   table: {
@@ -28,16 +30,57 @@ const SuccessfullBooking = () => {
   const [flightOrder, setFlightOrder] = useState(undefined);
   const [fareRules, setFareRules] = useState(undefined);
 
+  const router = useRouter();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const alertPop = (message, type) => {
+    enqueueSnackbar(message, {
+      anchorOrigin: {
+        vertical: "top",
+        horizontal: "center",
+      },
+      variant: type,
+    });
+  };
+
+
+
   useEffect(() => {
     if (typeof window !== "undefined") {
-      setLocal(JSON.parse(window.localStorage.getItem("local")));
       setFlightOffer(
         JSON.parse(window.localStorage.getItem("bookedFlightOffer"))
       );
       setFlightOrder(JSON.parse(window.localStorage.getItem("flightOrder")));
-      setFareRules(JSON.parse(window.localStorage.getItem("fareRules")));
+      const fareRules = window.localStorage.getItem("fareRules");
+      if (fareRules !== "undefined") setFareRules(JSON.parse(fareRules));
     }
   }, [null]);
+
+  useEffect(() => {
+    //this test for stale and undefined data
+    if (typeof window !== "undefined") {
+      const local = JSON.parse(window.localStorage.getItem("local"));
+      if (local === "undefined") {
+        alertPop(
+          "you have not done any search, please search for a flight on the homepage",
+          "info"
+        );
+        setTimeout(() => {
+          router.push("/");
+        }, 3000);
+      }
+      const departureDate = local.prevState.departureDate;
+      if (new Date(departureDate).getTime() < new Date().getTime()) {
+        alertPop(
+          "travel date is in the past, redirecting to hompage",
+          "info"
+        );
+        setTimeout(() => {
+          router.push("/");
+        }, 3000);
+      }
+      setLocal(local);
+    }
+  }, []);
 
   console.log("sucess fare rules", fareRules);
   return (
